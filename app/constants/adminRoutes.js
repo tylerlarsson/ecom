1cc418a9f4759a2db34913a7f79915e9e338b0ac
Tyler Larsson <tylerlarsson220@gmail.com>
2019-11-19 21:56:25 +0200

    server side: Test data for staging

--- app/constants/adminRoutes.js
+++ app/constants/adminRoutes.js
@@ -2,78 +2,123 @@
 // import Dashboard from '@material-ui/icons/Dashboard';
 import Person from '@material-ui/icons/Person';
 import Dashboard from '@material-ui/icons/Dashboard';
+import Ballot from '@material-ui/icons/Ballot';
+import People from '@material-ui/icons/People';
+import Web from '@material-ui/icons/Web';
+import VideoLibrary from '@material-ui/icons/VideoLibrary';
+import TrendingUp from '@material-ui/icons/TrendingUp';
+import SettingsApplications from '@material-ui/icons/SettingsApplications';
 // core components/views for Admin layout
 // import UserProfile from 'views/UserProfile/UserProfile.jsx';
+import HomePage from 'containers/HomePage';
 import Users from 'containers/Users/Users';
 import Roles from 'containers/Roles/Roles';
 import Role from 'containers/Roles/Role';
+import ProfilePage from 'containers/ProfilePage';
 import Permissions from 'containers/Permissions/Permissions';
 import routes from 'constants/routes.json';
 import UsersFilterPage from 'containers/UsersFilterPage/UsersFilterPage';
-import DashboardPage from 'containers/HomePage';
 
 const dashboardRoutes = [
   {
-    path: '/',
+    path: routes.DASHBOARD,
     name: 'Dashboard',
     icon: Dashboard,
-    component: DashboardPage,
-    layout: '/admin'
-  },
-  // {
-  //   path: '/user',
-  //   name: 'User Profile',
-  //   icon: Person,
-  //   component: UserProfile,
-  //   layout: '/admin'
-  // },
-  {
-    path: routes.FILTER,
-    link: routes.FILTER.replace(':role', 'admin'),
-    name: 'Admin',
-    icon: Person,
-    component: UsersFilterPage,
-    layout: routes.ADMIN,
-    visible: true
-  },
-  {
-    path: routes.FILTER,
-    link: routes.FILTER.replace(':role', 'student'),
-    name: 'Students',
-    icon: Person,
-    component: UsersFilterPage,
+    component: HomePage,
     layout: routes.ADMIN,
     visible: true
   },
   {
     path: routes.USERS,
     name: 'Users',
-    icon: Person,
+    icon: People,
     component: Users,
     layout: routes.ADMIN,
+    visible: true,
+    children: [
+      {
+        path: routes.FILTER,
+        link: routes.FILTER.replace(':role', 'admin'),
+        name: 'Admins',
+        icon: People,
+        component: UsersFilterPage,
+        layout: routes.ADMIN,
+        visible: true
+      },
+      {
+        path: routes.FILTER,
+        link: routes.FILTER.replace(':role', 'student'),
+        name: 'Students',
+        icon: People,
+        component: UsersFilterPage,
+        layout: routes.ADMIN,
+        visible: true
+      }
+    ]
+  },
+  {
+    path: routes.SITE,
+    name: 'Site',
+    icon: Web,
+    component: HomePage,
+    layout: routes.ADMIN,
     visible: true
   },
   {
-    path: routes.ROLE,
-    name: 'Role',
-    icon: Person,
-    component: Role,
-    layout: routes.ADMIN,
-    visible: false
-  },
-  {
-    path: routes.ROLES,
-    name: 'Roles',
-    icon: Person,
-    component: Roles,
+    path: routes.SALES,
+    name: 'Sales',
+    icon: TrendingUp,
+    component: HomePage,
     layout: routes.ADMIN,
     visible: true
   },
   {
-    path: routes.PERMISSIONS,
-    name: 'Permissions',
+    path: routes.SETTINGS,
+    name: 'Settings',
+    icon: SettingsApplications,
+    component: HomePage,
+    layout: routes.ADMIN,
+    visible: true,
+    children: [
+      {
+        path: routes.ROLE,
+        name: 'Role',
+        icon: Person,
+        component: Role,
+        layout: routes.ADMIN,
+        visible: false
+      },
+      {
+        path: routes.ROLES,
+        name: 'Roles',
+        icon: Person,
+        component: Roles,
+        layout: routes.ADMIN,
+        visible: true
+      },
+      {
+        path: routes.PERMISSIONS,
+        name: 'Permissions',
+        icon: Ballot,
+        component: Permissions,
+        layout: routes.ADMIN,
+        visible: true
+      }
+    ]
+  },
+  {
+    path: routes.COURSES,
+    name: 'Courses',
+    icon: VideoLibrary,
+    component: HomePage,
+    layout: routes.ADMIN,
+    visible: true
+  },
+  {
+    path: routes.PROFILE,
+    name: 'Profile',
     icon: Person,
-    component: Permissions,
+    component: ProfilePage,
     layout: routes.ADMIN,
     visible: true
   }
--- server/filter/is-an-admin.filter.js
+++ server/filter/is-an-admin.filter.js
@@ -4,7 +4,6 @@ module.exports = {
   label: 'Is an admin',
   type: 'boolean',
   order: 140,
-  
   async filter() {
     const id = await db.model.Role.mapOneToId('admin');
     return { roles: id || null };
