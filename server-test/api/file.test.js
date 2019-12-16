a9f08e7f2dc27f3a3a996ad2f8457248d1985444
Tyler Larsson <tylerlarsson220@gmail.com>
2019-12-17 00:06:49 +0200

    server side: File urls fixes, and tests for pricing plans and files

--- server-test/api/file.test.js
+++ server-test/api/file.test.js
@@ -1,20 +1,37 @@
-// const HttpStatus = require('http-status-codes');
-// const request = require('supertest');
-// const config = require('../../server/config');
-// const app = require('../../server/web-server');
-// const db = require('../../server/db/test');
-// const path = `${config.get('base-path')}/file`;
+const HttpStatus = require('http-status-codes');
+const request = require('supertest');
+const config = require('../../server/config');
+const app = require('../../server/web-server');
+const path = `${config.get('base-path')}/file`;
 
 describe('file api test', () => {
   // POST /file/image
-  test('should raise error if invalid schema');
-  test('should raise error if cant recognize content type');
-  test('');
-  test('');
-
-  // DELETE /file/image
-  test(``);
-  test(``);
-  test('');
-  test('');
+  test('should raise error if invalid schema', async () => {
+    const res = await request(app)
+      .post(`${path}/image`)
+      .send({ image: 12345 });
+    expect(res.status).toBe(HttpStatus.UNPROCESSABLE_ENTITY);
+  });
+  test('should raise error if cant recognize content type', async () => {
+    const res = await request(app)
+      .post(`${path}/image`)
+      .send({ image: 'example.hfgohb' });
+    expect(res.status).toBe(HttpStatus.UNPROCESSABLE_ENTITY);
+  });
+  test('should raise error if not an image', async () => {
+    const res = await request(app)
+      .post(`${path}/image`)
+      .send({ image: 'test.doc' });
+    expect(res.status).toBe(HttpStatus.UNPROCESSABLE_ENTITY);
+  });
+  test('should return link', async () => {
+    const res = await request(app)
+      .post(`${path}/image`)
+      .send({ image: 'test.png' });
+    expect(res.status).toBe(HttpStatus.OK);
+    // eslint-disable-next-line no-useless-escape
+    const expression = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+    const regex = new RegExp(expression);
+    expect(regex.test(res.body.url)).toBe(true);
+  });
 });
