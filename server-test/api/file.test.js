d54d2f896e10a1980b90f100457bd3c27cfe9a11
Tyler Larsson <tylerlarsson220@gmail.com>
2019-12-16 23:48:40 +0200

    server side: New schemas

---
+++ server-test/api/file.test.js
@@ -0,0 +1,20 @@
+// const HttpStatus = require('http-status-codes');
+// const request = require('supertest');
+// const config = require('../../server/config');
+// const app = require('../../server/web-server');
+// const db = require('../../server/db/test');
+// const path = `${config.get('base-path')}/file`;
+
+describe('file api test', () => {
+  // POST /file/image
+  test('should raise error if invalid schema');
+  test('should raise error if cant recognize content type');
+  test('');
+  test('');
+
+  // DELETE /file/image
+  test(``);
+  test(``);
+  test('');
+  test('');
+});
