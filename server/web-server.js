286aefbbf5c61b2a6bdcfa7c8dc393a2c161428f
Tyler Larsson <tylerlarsson220@gmail.com>
2019-12-23 20:50:47 +0200

    server side: Web server

--- server/web-server.js
+++ server/web-server.js
@@ -1,6 +1,4 @@
 const path = require('path');
-const spdy = require('spdy');
-const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const swaggerUi = require('swagger-ui-express');
@@ -30,18 +28,16 @@ const port = config.get('web-app:port');
 /* istanbul ignore next */
 if (config.get('NODE_ENV') === 'production') {
   // production mode
-  const spdyOptions = {
-    key: fs.readFileSync(''), // have to add in production env
-    cert: fs.readFileSync(''), // have to add in production env
-    spdy: {
-      requestCert: false,
-      protocols: ['h2', 'spdy/3.1', 'spdy/3', 'spdy/2', 'http/1.1', 'http/1.0']
-    }
-  };
+  // const spdyOptions = {
+  //   key: fs.readFileSync(''), // have to add in production env
+  //   cert: fs.readFileSync(''), // have to add in production env
+  //   spdy: {
+  //     requestCert: false,
+  //     protocols: ['h2', 'spdy/3.1', 'spdy/3', 'spdy/2', 'http/1.1', 'http/1.0']
+  //   }
+  // };
   app.use('/', express.static(path.join(__dirname, '..', 'build')));
-  spdy.createServer(spdyOptions, app).listen(port, () => {
-    logger.info('web server started in production, port', port);
-  });
+  app.listen(port, () => logger.info('web server started in production, port', port));
 } else if (config.get('NODE_ENV') === 'development') {
   // npm: run dev script to start
   // then web-server
